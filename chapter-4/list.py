f68cb658274a81a36698449d2f54316b3e5e3ace
Umm e Amara <uammara650@gmail.com>
2025-03-24 19:43:42 +0500

    chpater-4

---
+++ chapter-4/list.py
@@ -0,0 +1,83 @@
+#Python lists are containers to store set of values of any data type.
+# f=["amara","harry",7,8.09,False]
+# print(f)
+# print(f[3])
+# f[0]="haris"
+# print(f) #Unlike strings lists are mmutable means they can be change
+# print(f[1:4])
+# f.append("rina")
+# print(f)
+#       #              ...........................................
+
+#List functions/methods
+# l=[4,78,9,5,45,87]
+#print(sum(l)) #sum list elements and give answers
+# l.sort()
+# print(f"Sorted list:{l}")
+# l.reverse()
+# print(f"Reverse list:{l}")
+# l.insert(3,8999) #8999 will be inserted at position 3
+# print(f"Insert in list:{l}")
+# l.pop(4) #means position 4 will be deleted
+# print(f"Remove position from list:{l}")
+# l.remove(4) #means element 4 will be deleted
+# print(f"Remove element from list:{l}")
+#       #              ...........................................
+
+# Tuple 
+# a=(1,5,7,8,9,True,"Amara") #tuples are immutable
+# print(a)
+# print(type(a))
+
+#       #              ...........................................
+
+#Tuple method 
+# a=(1,5,7,8,9,True,"Amara") 
+# no=a.count(7) #If 7 is in tuple then tell how many time it exist in tuple
+# print(no)
+# i=a.index(7) # 7 at which index
+# print(i)
+# #Also go to chatgpt for more tuple methods
+#       #              ...........................................
+
+#Program to print 7 friuts by taking input from user
+# fruit=[]
+# f1=input("Enter fruit name:")
+# fruit.append(f1)
+# f2=input("Enter fruit name:")
+# fruit.append(f2)
+# f3=input("Enter fruit name:")
+# fruit.append(f3)
+# f4=input("Enter fruit name:")
+# fruit.append(f4)
+# f5=input("Enter fruit name:")
+# fruit.append(f5)
+# f6=input("Enter fruit name:")
+# fruit.append(f6)
+# f7=input("Enter fruit name:")
+# fruit.append(f7)
+# print("Fruits are:")
+# print (fruit)
+#       #              ...........................................
+
+#Program to print marks of 6 students in sorted manner by taking input from user
+# marks=[]
+# f1=int(input("Enter marks:"))
+# marks.append(f1)
+# f2=int(input("Enter marks:"))
+# marks.append(f2)
+# f3=int(input("Enter marks:"))
+# marks.append(f3)
+# f4=int(input("Enter marks:"))
+# marks.append(f4)
+# f5=int(input("Enter marks:"))
+# marks.append(f5)
+# f6=int(input("Enter marks:"))
+# marks.append(f6)
+# marks.sort()
+# print("Marks in sorted manner  are:")
+# print (marks)
+#print(sum(marks))
+
+#       #              ...........................................
+
